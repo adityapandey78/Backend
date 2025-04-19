d72b3f8fb5fcf868a7d8b22f8d62c6ed16078fa6
Aditya Pandey <140270777+adityapandey78@users.noreply.github.com>
2025-04-19 17:19:57 +0530

    Completed Aync Await for fs module

--- Lect13-15/fsAsyncAwait.js
+++ Lect13-15/fsAsyncAwait.js
@@ -1,10 +1,10 @@
-const fs= require("fs");
+const fs= require("fs/promises");
 const path = require("path");
 
 const fileName='asyntFile.txt';
 const filePath= path.join(__dirname,fileName);
 
-const filePath1=__dirname;
+// const filePath1=__dirname;
 // fs.promises
 //    .readdir(filePath1)
 //     .then((data)=>console.log(data))
@@ -23,3 +23,43 @@ const filePath1=__dirname;
 // }
 // readFolder();
 
+ 
+//Writing a file or cre4ating it
+
+// const writeFileExample = async()=>{
+//     try {
+//        await fs.writeFile(filePath,'This is the intial data by Pandey','utf-8');
+//        console.log("File Created Successfully");
+        
+//     } catch (error) {
+//         console.log("Error in writing file",error);
+        
+//     }
+// }
+// writeFileExample();
+
+
+// const readFileExample = async()=>{
+//     try {
+//        const data =await fs.readFile(filePath,'utf-8');
+//        console.log("File Read Successfully");
+//        console.log(data.toString()); 
+        
+//     } catch (error) {
+//         console.log("Error in reading file",error);
+        
+//     }
+// }
+// readFileExample();
+
+const appendFileExample = async()=>{
+    try {
+       const data =await fs.appendFile(filePath,'\nThis is the appended data by Pandey','utf-8');
+       console.log("File Appended Successfully\n Added data is: ",data.toString());
+        
+    } catch (error) {
+        console.log("Error in writing file",error);
+        
+    }
+}
+appendFileExample();
